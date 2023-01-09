6397defc3cfbedf052b67ddbc0de1a6e304469e5
Abhishekkjx <117756312+Abhishekkjx@users.noreply.github.com>
2023-01-09 17:33:20 +0530

    Add files via upload
    This is in progress.

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+var navbar=document.querySelector(".navbar");
+window.onscroll=()=>{
+    this.scrollY>20?navbar.classList.add("sticky"): navbar.classList.remove("sticky");
+}
+/* skills */
+const skills_wrap = document.querySelector(".about-skills"),
+      skills_bar = document.querySelectorAll(".progress-line");
+      window.addEventListener("scroll",() => {
+            skillsEffect();
+      })
+      function checkScroll(el) {
+        let rect = el.getBoundingClientRect();
+        if(window.innerHeight >= rect.top + el.offsetHeight) return true;
+        return false;
+      }
+      function skillsEffect(){
+        if(!checkScroll(skills_wrap)) return;
+        skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
+      }
